25b973de925a6cd4007e1e90f1785938be2c91e5
Brian Speight <brspeight@expedia.com>
2018-10-26 12:00:38 -0700

    removing defaults and adding the Orion header and c3 components

---
+++ src/components/C3/C3Container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './C3Container.css';
+
+class C3Container extends Component {
+  popUpContainer() {}
+  render() {
+    return (
+      <div className="c3-container">
+        <header className="c3-container__header">
+          <h1>Help</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default C3Container;
---
+++ src/components/OrionHeader/OrionHeader.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import VoyagerLogo from '../VoyagerLogo/VoyagerLogo.js';
+import './OrionHeader.css';
+import { Object } from 'core-js';
+
+class OrionHeader extends Component {
+  // Select all items in the parent container of clicked item and remove 'is_active'
+  // then add 'is_active' to the clicked item.
+  makeActive(event) {
+    const allSiblings = event.target.parentElement.childNodes;
+    for (var i = 0; i < allSiblings.length; i++) {
+      allSiblings[i].classList.remove('is_active');
+    }
+    event.target.classList.add('is_active');
+  }
+  // Select the first item in the 'mainNav' container and make it active
+  defaultActive() {
+    let firstNavItem = document.querySelector('.mainNav').firstChild;
+    firstNavItem.classList.add('is_active');
+  }
+  // Render it up!
+  render() {
+    console.log(this.props);
+    const navItems = Object.keys(this.props.globalNav).map(i => {
+      return (
+        <li className="mainNavItem" onClick={this.makeActive} key={i}>
+          {this.props.globalNav[i]}
+        </li>
+      );
+    });
+
+    return (
+      <div className="activeArea">
+        <div className="logoNav">
+          <VoyagerLogo />
+          <ul className="mainNav">{navItems}</ul>
+        </div>
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.defaultActive();
+  }
+}
+
+export default OrionHeader;
