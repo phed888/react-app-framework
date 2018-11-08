a468d23909a1358f35962358a47574021a70fe86
Brian Speight <brspeight@expedia.com>
2018-11-07 17:32:55 -0800

    removing c3 adding testpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import OrionHeader from './components/OrionHeader';
-import C3Container from './components/C3Container';
 import './App.scss';
+import TestPage from './components/TestPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <OrionHeader globalNav={['Tasks', 'History', 'Statistics', 'Team']} />
-        <C3Container />
+        <TestPage />
       </div>
     );
   }
