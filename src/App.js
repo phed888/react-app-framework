59d0e8690f45e7b426b41152474ecd15c0e99d68
Brian Speight <brspeight@expedia.com>
2018-11-08 17:23:05 -0800

    changing test page for getting started

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import OrionHeader from './components/OrionHeader';
 import './App.scss';
-import TestPage from './components/TestPage';
+import GettingStarted from './components/GettingStarted';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <OrionHeader globalNav={['Tasks', 'History', 'Statistics', 'Team']} />
-        <TestPage />
+        <GettingStarted />
       </div>
     );
   }
